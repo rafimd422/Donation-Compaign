431eeceeefe82da0a22f1da8a4d5aeb6b160ff6f
rafimd422 <rafimf2222@gmail.com>
2023-09-25 14:28:14 +0600

    basic file setup complated

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+      // ...
+      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
+  ]
+,
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+]
+}
+
