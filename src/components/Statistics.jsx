643b8be902fce9b1da6977844737a6e1b8d389f8
rafimd422 <rafimf2222@gmail.com>
2023-09-26 22:35:43 +0600

    Solved my mistakes in chart. added a chart on the statistics page. using recharts

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,46 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { useLoaderData } from 'react-router-dom'
-import { getDonation } from './Home/utility/localstorage'
-import { PieChart, Pie, Tooltip } from 'recharts';
-
-
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { getDonation } from './Home/utility/localstorage';
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
-    const [mainData, setMainData] = useState([])
-    const [value, setValue] = useState(0)
-    const [totalPrice, setTotalPrice] = useState(0)
-    const data = useLoaderData()
+  const [mainData, setMainData] = useState([]);
+  const [allData, setAllData] = useState([])
 
-    useEffect(()=>{
-        const storedData = getDonation()
-        if(data.length > 0){
-        const filteredData = data.filter(mainData => storedData.includes(mainData.id))
-        setMainData(filteredData)
-        const mapedData = mainData.map(data => data.price)
-        const sum = mapedData.reduce((acc, current) => acc + current, 0);
-        const totalValue = 2980;
-        setTotalPrice(totalValue)
-        const percentAge = (sum/totalPrice)*100
-        setValue(percentAge)
+  const jsonData = useLoaderData();
+
+  useEffect(() => {
+
+fetch('donation.json')
+.then(res => res.json())
+.then(allJsonData => setAllData(allJsonData))
+
+
+
+    const storedData = getDonation();
+    if (jsonData.length > 0) {
+      const filteredData = jsonData.filter((data) => storedData.includes(data.id));
+      setMainData(filteredData);
     }
-     },[])
- const chartData = [
-    {name:'Your Donation' , value: `${value}%`},
-    {name:'Total Donation', value: totalPrice}
-]
+  }, []);
+
+  const mainDataLength = mainData.length;
+  const allDataLength = allData.length;
+
+  const data = [
+    { name: 'Your Donation', value: mainDataLength },
+    { name: 'Total Donation', value: allDataLength },
+  ];
+
+  const COLORS = ['#FF0000', '#00C49F'];
+
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
 
   return (
-<div className='text-center mx-auto'>
-<PieChart width={760} height={270} className='mx-auto' >
-  <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-  <Pie data={chartData} dataKey="value" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
-  <Tooltip />
-</PieChart>
-<span >Total Donation  <span className='bg-[#82ca9d] text-[#82ca9d] rounded-sm w-4 h-1'>---   .</span> </span>
-<span>Your Donation  <span className='bg-[#8884d8] text-[#8884d8] rounded-sm w-4 h-1'>---   .</span> </span>
+    <div className="text-center mx-auto">
+      <ResponsiveContainer width="100%" height={500}>
+        <PieChart>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={120}
+            fill="#8884d8"
+            dataKey="value"
+          >
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
 
-</div>
-  )
-}
+      <span className='me-4'>Total Donation  <span className='bg-[#00C49F] text-[#00C49F] rounded-sm w-4 h-1'>---   .</span> </span>
+      <span>Your Donation  <span className='bg-[#FF0000] text-[#FF0000] rounded-sm w-4 h-1'>---   .</span> </span>
+    </div>
+  );
+};
 
-export default Statistics
+export default Statistics;
